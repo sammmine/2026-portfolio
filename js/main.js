6a2a8fcfbd0da9a9d535b1cdb4067dbcda3d57c7
Samantha Laqueenna Ginting <167011131+sammmine@users.noreply.github.com>
2026-09-07 13:24:42 +0700

    Initial commit: 2026 portfolio

---
+++ js/main.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll(".nav_link");
+    const menuToggle = document.getElementById("menu-toggle");
+    const filterButtons = document.querySelectorAll(".project_filters .filter");
+    const projectCards = document.querySelectorAll(".grid .card");
+
+    // Close hamburger menu if clicked
+    navLinks.forEach((link) => {
+        link.addEventListener("click", () => {
+            if (menuToggle && menuToggle.checked) {
+                menuToggle.checked = false;
+            }
+        });
+    });
+
+    // Filter
+    filterButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            filterButtons.forEach((btn) => btn.classList.remove("active"));
+            button.classList.add("active");
+
+            const selectedFilter = button.getAttribute("data-filter");
+
+            projectCards.forEach((card) => {
+                const cardTag = card.getAttribute("data-tags");
+                if (selectedFilter === "all" || cardTag === selectedFilter) {
+                    card.style.display = "block";
+                } else {
+                    card.style.display = "none";
+                }
+            });
+        });
+    });
+});
